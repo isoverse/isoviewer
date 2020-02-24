7d2514dae96afd04970965e4f2a5a984e7204873
Sebastian Kopf <sebastian.kopf@colorado.edu>
2020-02-24 00:57:33 -0700

    function to find iso objects in workspace added

--- R/utils.R
+++ R/utils.R
@@ -7,3 +7,21 @@ wrap_function_name <- function(string, max_length, divider = "_", newline = "\n"
   cuts <- map(nchars, ~c(0, diff(floor(.x/max_length))))
   map2_chr(cuts, pieces, ~str_c(str_c(ifelse(.x == 1, newline, ""), .y), collapse = divider))
 }
+
+
+# find iso objects in the global environment
+find_iso_objects <- function() {
+
+  iso_objs <- rlang::set_names(ls(envir = .GlobalEnv))
+  iso_di <- purrr::map_lgl(iso_objs, ~isoreader::iso_is_dual_inlet(get(.x)))
+  iso_cf <- purrr::map_lgl(iso_objs, ~isoreader::iso_is_continuous_flow(get(.x)))
+  iso_scan <- purrr::map_lgl(iso_objs, ~isoreader::iso_is_scan(get(.x)))
+
+  return(
+    list(
+      di = sort(names(iso_di[iso_di])),
+      cf = sort(names(iso_cf[iso_cf])),
+      scan = sort(names(iso_scan[iso_scan]))
+    )
+  )
+}
