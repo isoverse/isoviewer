5ad1092f3b378a39f6aef899e63086d9e104fb67
Sebastian Kopf <sebastian.kopf@colorado.edu>
2020-02-24 00:58:16 -0700

    add gui settings functions

--- R/settings.R
+++ R/settings.R
@@ -21,3 +21,29 @@ turn_debug_on <- function() {
 turn_debug_off <- function(data) {
   set_setting("debug", FALSE)
 }
+
+# gui settings
+set_gui_setting <- function(name, value) {
+  if (are_gui_settings_on()) set_setting(paste0("gui.", name), value)
+}
+
+# returns gui setting if gui settings are on and the setting is available
+# otherwise returns the default
+get_gui_setting <- function(name, default = NULL) {
+  if (!are_gui_settings_on()) return(default)
+  value <- getOption(paste0("isoviewer.gui.", name))
+  if (is.null(value)) return(default)
+  return(value)
+}
+
+turn_gui_settings_on <- function() {
+  set_setting("gui.settings", TRUE)
+}
+
+turn_gui_settings_off <- function() {
+  set_setting("gui.settings", FALSE)
+}
+
+are_gui_settings_on <- function() {
+  setting("gui.settings")
+}
--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,8 @@
 .onLoad <- function(libname, pkgname) {
   # set default package options
   default_options <- list(
-    isoviewer.debug = FALSE
+    isoviewer.debug = FALSE,
+    isoviewer.gui.settings = TRUE
   )
   options(default_options)
   invisible()
